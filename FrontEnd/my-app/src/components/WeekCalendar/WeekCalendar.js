50fc820ebe7522a69ecf05a449887dd09f51087c
Julian Bitetto s3690935 <s3690935@student.rmit.edu.au>
2020-09-01 12:44:21 +1000

    Created weekCalendar branch

---
+++ FrontEnd/my-app/src/components/WeekCalendar/WeekCalendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class WeekCalendar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default WeekCalendar
