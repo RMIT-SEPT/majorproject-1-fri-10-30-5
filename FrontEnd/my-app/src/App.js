50f359c77e7b6c3b87efea17cdeab41c1f0b0a96
Julian Bitetto s3690935 <s3690935@student.rmit.edu.au>
2020-09-01 23:07:58 +1000

    Created calendar in react

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router,Route } from 'react-router-dom'
-import Searchbar from './components/Searchbar/Searchbar'
-import SearchPage from './components/SearchPage/SearchPage'
+import WeekCalendar from './components/WeekCalendar/WeekCalendar'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Route path="/dashboard" component={ Searchbar } />
-          <Route path="/searchResults" component={ SearchPage } />
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+        <WeekCalendar />
+    </div>
   );
 }
 
