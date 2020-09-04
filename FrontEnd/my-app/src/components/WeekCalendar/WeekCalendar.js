ac1855dc2e8b6b8da1df8c784564bbc272b3d9d2
Julian Bitetto <julian.bitetto@gmail.com>
2020-09-04 21:21:35 +1000

    Array state changes

--- FrontEnd/my-app/src/components/WeekCalendar/WeekCalendar.js
+++ FrontEnd/my-app/src/components/WeekCalendar/WeekCalendar.js
@@ -2,74 +2,99 @@ import React, { Component } from 'react'
 import './WeekCalendar.css'
 import DataTable from 'react-data-table-component';
 import moment from 'moment'
- 
-/*
-  Object returned by querying
-  WorkerID + all shifts for the next 7 days
-*/
-const res = [
-  {
-    startTime: "13:00",
-    endTime: "14:00",
-    date: "3/8/2020"
-  },
-  {
-    startTime: "15:00",
-    endTime: "16:00",
-    date: "5/8/2020"
-  },
-  {
-    startTime: "14:00",
-    endTime: "15:00",
-    date: "6/8/2020"
-  }
-];
-
-const data = [];
-function convertData(data) {
-
-  for(let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-
-    // data.push({
-    //   i: "hello"
-    // },);
-  }
-};
-
-// Convert the database result into an interpretable 
-// object for the data table
-convertData(res);
 
 class WeekCalendar extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      data: ['test'],
+
+      /*
+      Object returned by querying
+      WorkerID + all shifts for the next 7 days
+      */
+      res: [{
+        startTime: "13:00",
+        endTime: "14:00",
+        date: "8/3/2020"
+      },
+      {
+        startTime: "15:00",
+        endTime: "16:00",
+        date: "8/5/2020"
+      },
+      {
+        startTime: "14:00",
+        endTime: "15:00",
+        date: "8/16/2020"
+      }]
+    };
+
+    // Convert the database result into an interpretable 
+    // object for the data table
+    // console.log(this.state.res)
+    this.convertData(this.state.res)
+    
+  
+  }
+
+  convertData(dataSet) {
+
+    // console.log(dataSet)
+
+    let newData = [];
+  
+    for(let i = 0; i < dataSet.length; i++) {
+  
+      let tmp = moment(dataSet[i]["date"]).format("D/M/YYYY").substr(0,2);
+  
+      if(parseInt(tmp) > 10) {
+        tmp = moment(dataSet[i]["date"]).format("D/M/YYYY").substr(0,4);
+      }
+      else {
+        tmp = moment(dataSet[i]["date"]).format("D/M/YYYY").substr(0,3);
+      }
+  
+      newData.push(tmp);
+    }
+  
+    // console.log(newData)
+    this.setState({data: newData})
+    
+  };
+
   render() {
+
+    // Columns in the table
+    // Each column is the date of the next seven days + current day
+    const columns = [
+      {
+        name: "Today",
+        selector: String(moment().date() + 0 + "/" + moment().month()),
+        sortable: false,
+      },
+    ];
+
+    for(let i = 1; i < 8; i++) {
+      columns.push({
+        name: moment().date() + i + "/" + moment().month(),
+        selector: String(moment().date() + i + "/" + moment().month()),
+        sortable: false,
+      },);
+    }
+
+    console.log(this.state.data)
 
     return (
       <DataTable
         title="This Week"
         columns={columns}
-        data={data}
+        data={this.state.data}
       />
     )
   }
 };
-
-// Columns in the table
-// Each column is the date of the next seven days + current day
-const columns = [
-  {
-    name: "Today",
-    selector: String(moment().date() + 0 + "/" + moment().month()),
-    sortable: false,
-  },
-];
-
-for(let i = 1; i < 8; i++) {
-  columns.push({
-    name: moment().date() + i + "/" + moment().month(),
-    selector: String(moment().date() + i + "/" + moment().month()),
-    sortable: false,
-  },);
-}
 
 export default WeekCalendar
