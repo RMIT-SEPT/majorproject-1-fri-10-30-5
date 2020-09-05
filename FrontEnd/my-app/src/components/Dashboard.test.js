aca43ec0b7192c22c80cfccc8e3a1f0bb63885ae
Amna Alfarah Alonto <s3778713@student.rmit.edu.au>
2020-09-05 22:24:53 +1000

    Minor Header changes and Testing done

---
+++ FrontEnd/my-app/src/components/Dashboard.test.js
@@ -0,0 +1,52 @@
+import React from "react";
+import SearchPageButton from './SearchPage/SearchPageButton';
+import ProfileButton from './Profile/ProfileButton';
+import PastBookingsButton from './Bookings/PastBookingsButton';
+import UpcomingBookingsButton from './Bookings/UpcomingBookingsButton';
+import {shallow, mount} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+describe('render Dashboard buttons', () => {
+    it("should render button as Make A Booking", ()=>{
+        const wrapper = shallow(<SearchPageButton/>);
+        const pageHeading = wrapper.find('Link').first().text();
+        expect(pageHeading).toEqual('Make a Booking');
+    });
+
+    it("should render button as View Profile", ()=>{
+        const wrapper = shallow(<ProfileButton/>);
+        const pageHeading = wrapper.find('Link').first().text();
+        expect(pageHeading).toEqual('View Profile');
+    });
+
+    it("should render button as View Profile", ()=>{
+        const wrapper = shallow(<ProfileButton/>);
+        const pageHeading = wrapper.find('Link').first().text();
+        expect(pageHeading).toEqual('View Profile');
+    });
+
+    it("should render button as View Past Bookings", ()=>{
+        const wrapper = shallow(<PastBookingsButton/>);
+        const pageHeading = wrapper.find('Link').first().text();
+        expect(pageHeading).toEqual('View Past Bookings');
+    });
+
+    it("should render button as View Upcoming Bookings", ()=>{
+        const wrapper = shallow(<UpcomingBookingsButton/>);
+        const pageHeading = wrapper.find('Link').first().text();
+        expect(pageHeading).toEqual('View Upcoming Bookings');
+    })
+
+
+
+
+
+
+
+    //test log console - one way of testing console.log(components)
+})
+
--- FrontEnd/my-app/src/components/Layout/Header.js
+++ FrontEnd/my-app/src/components/Layout/Header.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react'
+import styles from '../../css/Styles.css'
+
 
  class Header extends Component {
     render() {
         return (
             <div>
-            <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+            <nav className = "navbar navbar-expand-md navbar-light">
             <div className="container">
-                <a className="navbar-brand" href="/homepage">
-                    AGME
+                <a className="navbar-brand" href="/dashboard">
+                <img src = {styles.icon}/>
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
                     <span className="navbar-toggler-icon" />
                 </button>
     
                 <div className="collapse navbar-collapse" id="mobile-nav">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/dashboard">
-                                Dashboard
-                            </a>
-                        </li>
-                    </ul>
-    
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <a className="nav-link " href="register">
