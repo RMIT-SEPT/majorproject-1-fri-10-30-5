ffe9e369fdf45a354f77f98a75c0b15021d69b96
Julian Bitetto s3690935 <s3690935@student.rmit.edu.au>
2020-09-10 19:33:36 +1000

    React testing

---
+++ FrontEnd/my-app/src/test/SearchBar.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {shallow} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import Searchbar from '../components/SearchAvailability/Searchbar'
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+describe('Render <Searchbar /> component', () => {
+  
+    it("Should render a single <Searchbar /> component", ()=>{
+        const component = shallow(<Searchbar />);
+        expect(component).toHaveLength(1);
+    });
+})
+
---
+++ FrontEnd/my-app/src/test/SearchPageButton.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {shallow} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Sinon from 'sinon'
+
+import SearchPageButton from '../components/SearchAvailability/SearchPageButton'
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+describe('Render <SearchPageButton /> component', () => {
+  
+    it("render a single <SearchPageButton /> component", ()=>{
+        const component = shallow(<SearchPageButton />);
+        expect(component).toHaveLength(1);
+    });
+
+    it("simulate click event", ()=>{
+        const onButtonClick = Sinon.spy();
+        const component = shallow(<SearchPageButton onButtonClick={onButtonClick} />);
+        component.simulate('click');
+        expect(onButtonClick).toHaveProperty('callCount', 1);
+    });
+})
+
