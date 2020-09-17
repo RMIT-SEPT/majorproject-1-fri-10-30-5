95dc930e0387862b473b9fe1436a8718b04ba184
s3781183 <rushebala@gmail.com>
2020-09-17 18:39:58 +1000

    admin controller testing added

--- BackEnd/src/main/java/com/rmit/sept/turtorial/demo/web/AdminController.java
+++ BackEnd/src/main/java/com/rmit/sept/turtorial/demo/web/AdminController.java
@@ -1,6 +1,8 @@
 package com.rmit.sept.turtorial.demo.web;
 
 import com.rmit.sept.turtorial.demo.model.Admin;
+import com.rmit.sept.turtorial.demo.model.Booking;
+import com.rmit.sept.turtorial.demo.model.WorkingHours;
 import com.rmit.sept.turtorial.demo.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,28 +29,38 @@ public class AdminController {
         return new ResponseEntity<String>(admin1, HttpStatus.CREATED);
     }
 
-    @PutMapping("/update")
+    @PutMapping("/update/{userName}")
     public ResponseEntity<?> updateAdmin(@Valid @RequestBody Admin admin, BindingResult result) {
         if (result.hasErrors()){
             return new ResponseEntity<String>("Invalid Admin Object", HttpStatus.BAD_REQUEST);
         }
         Admin admin1 = adminService.updateAdmin(admin);
-        return new ResponseEntity<Admin>(admin1, HttpStatus.CREATED);
+      //  if (admin1 != null){
+            return new ResponseEntity<Admin>(admin1, HttpStatus.OK);
+      //  }else{
+     //       return new ResponseEntity<String>("Booking Object Could Not Be Updated", HttpStatus.CONFLICT);
+     //   }
     }
 
     @DeleteMapping("/delete/{userName}")
     public ResponseEntity<?> deleteAdmin(@Valid @PathVariable String userName) {
         String admin1 = adminService.deleteAdmin(userName);
-        return new ResponseEntity<String>(admin1, HttpStatus.CREATED);
+    //    if (admin1 != null){
+            return new ResponseEntity<String>(admin1, HttpStatus.ACCEPTED);
+    //    }else{
+    //        return new ResponseEntity<String>("No Admin Object", HttpStatus.NOT_FOUND);
+    //    }
     }
 
     @GetMapping("/{userName}")
     public ResponseEntity<?> findAdmin(@Valid @PathVariable String userName) {
-//        if (result.hasErrors()){
-//            return new ResponseEntity<String>("Invalid Admin Object", HttpStatus.BAD_REQUEST);
-//        }
+
         Admin admin1 = adminService.getAdminByUserName(userName);
-        return new ResponseEntity<Admin>(admin1, HttpStatus.CREATED);
+      //  if (admin1 != null){
+            return new ResponseEntity<Admin>(admin1, HttpStatus.OK);
+     //   }else{
+     //       return new ResponseEntity<String>("No Admin Object", HttpStatus.NOT_FOUND);
+     //   }
     }
 }
 
--- BackEnd/src/test/java/com/rmit/sept/turtorial/demo/web/AdminControllerTest.java
+++ BackEnd/src/test/java/com/rmit/sept/turtorial/demo/web/AdminControllerTest.java
@@ -1,18 +1,17 @@
 package com.rmit.sept.turtorial.demo.web;
 
+
 import org.junit.Test;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rmit.sept.turtorial.demo.model.Admin;
 import com.rmit.sept.turtorial.demo.services.AdminService;
-import com.rmit.sept.turtorial.demo.web.AdminController;
-import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -20,40 +19,139 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 
-
-import java.util.Arrays;
-import java.util.List;
-
-//shorthand for springJunit4 class runner
-//
 @RunWith(SpringRunner.class)
 @WebMvcTest(AdminController.class)
 public class AdminControllerTest {
 
-    //sends post/gets to url etc.
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     private ObjectMapper objectMap;
 
-    //creating mock of working hour's service
     @MockBean
-    private  AdminService admin;
+    private AdminService adminService;
 
 
-
-
-
+    //return created status, when new admin is valid
     @Test
-    public void addAdmin() throws Exception {
-        Admin admin1 = new Admin("admin304","password","Admin","AdminLast","13 Fitz Street","0123456789");
+    public void givenValidAdmin_whenPostAdmin_thenSuccessfullyCreated() throws Exception {
+        Admin admin1 = new Admin("admin304", "password", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+
+        given(this.adminService.addAdmin(admin1)).willReturn(admin1.getUserName() +" added successfully");
 
         mvc.perform(post("/api/admin/add/").contentType(MediaType.APPLICATION_JSON)
                 .content(objectMap.writeValueAsString(admin1)))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isCreated());
-                //.andExpect(jsonPath("$.userName",is(admin1.getUserName())));
+               // .andExpect(jsonPath("$").doesNotExist());
+
     }
+
+    //return bad request when attempting to create admin with password less than 4 characters
+    @Test
+    public void givenInvalidAdminPassword_whenPostAdmin_thenBadRequest() throws Exception {
+        Admin admin1 = new Admin("admin304", "pas", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+        String invalidAdmin = "Invalid Admin Object";
+
+        given(this.adminService.addAdmin(admin1)).willReturn(invalidAdmin);
+
+        mvc.perform(post("/api/admin/add/").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMap.writeValueAsString(admin1)))
+                .andExpect(jsonPath("$",is(invalidAdmin)))
+                .andExpect(status().isBadRequest());
+    }
+
+    //return bad request when attempting to create admin with null lastName
+    @Test
+    public void givenNullAdminLastName_whenPostAdmin_thenBadRequest() throws Exception {
+        Admin admin1 = new Admin("admin304", "password", "Admin", null, "13 Fitz Street", "0123456789");
+        String invalidAdmin = "Invalid Admin Object";
+
+        given(this.adminService.addAdmin(admin1)).willReturn(invalidAdmin);
+
+        mvc.perform(post("/api/admin/add/").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMap.writeValueAsString(admin1)))
+                .andExpect(jsonPath("$",is(invalidAdmin)))
+                .andExpect(status().isBadRequest());
+    }
+
+    //return ok status, when updated admin is valid
+    @Test
+    public void givenValidAdmin_whenUpdateAdmin_thenSuccessfullyOk() throws Exception {
+        Admin admin1 = new Admin("admin304", "password", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+        Admin newAdmin = new Admin("admin304", "password123", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+
+        when(adminService.updateAdmin(newAdmin)).thenReturn(newAdmin);
+
+        mvc.perform(put("/api/admin/update/{userName}", admin1.getUserName()).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMap.writeValueAsString(newAdmin)))
+                .andExpect(status().isOk());
+    }
+
+    //return bad request when attempting to update admin with null password
+    @Test
+    public void givenNullAdminPassword_whenUpdateAdmin_thenBadRequest() throws Exception {
+        Admin admin1 = new Admin("admin30", "password", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+        Admin newAdmin = new Admin("admin304", null, "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+        String invalidAdmin = "Invalid Admin Object";
+
+        mvc.perform(put("/api/admin/update/{userName}", admin1.getUserName()).contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMap.writeValueAsString(newAdmin)))
+                .andExpect(jsonPath("$",is(invalidAdmin)))
+                .andExpect(status().isBadRequest());
+    }
+
+    //return accepted status, when deleting an existing admin
+    @Test
+    public void givenExistingAdmin_whenDeleteAdmin_thenSuccessfullyAccepted() throws Exception {
+        Admin admin1 = new Admin("admin30", "password", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+        String message = "Admin " + admin1.getUserName() + " has been successfully removed";
+
+        given(this.adminService.deleteAdmin(admin1.getUserName())).willReturn(message);
+
+        mvc.perform(delete("/api/admin/delete/{userName}", "admin30").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMap.writeValueAsString(admin1)))
+                .andExpect(jsonPath("$",is(message)))
+                .andExpect(status().isAccepted());
+        //     .andDo(MockMvcResultHandlers.print())
+    }
+
+
+    //return ok status, when searching for an existing admin
+    @Test
+    public void givenValidAdminUserName_whenGetAdmin_thenReturnAdmin() throws Exception {
+        Admin admin1 = new Admin("admin30", "password", "Admin", "AdminLast", "13 Fitz Street", "0123456789");
+
+        given(adminService.getAdminByUserName("admin30")).willReturn(admin1);
+
+        mvc.perform(get("/api/admin/{userName}", "admin30").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMap.writeValueAsString(admin1)))
+                .andExpect(status().isOk());
+        //     .andExpect(jsonPath("$.username",is(admin1.getUserName())))
+    }
+
+
+
+
+
+
 }
+
+//    @Test
+//    public void givenInvalidAdmin_whenDeleteAdmin_thenReturnNotFound() throws Exception {
+//        mvc.perform(delete("/api/admin/delete/{userName}", "admin30").contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isAccepted());
+//    }
+
+//    @Test
+//    public void givenInvalidAdminUserName_whenGetAdmin_thenReturnNotFound() throws Exception {
+//        mvc.perform(get("/api/admin/{userName}", "admin30").contentType(MediaType.APPLICATION_JSON));
+//        //     .andExpect(jsonPath("$.username",is(admin1.getUserName())))
+//
+//    }
