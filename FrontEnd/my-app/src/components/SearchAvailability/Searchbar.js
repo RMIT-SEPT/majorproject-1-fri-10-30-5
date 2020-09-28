dfbd1b23abebfdfbc04a7abfba6071ba1899a0fc
Julian <julian@192-168-1-130.tpgi.com.au>
2020-09-29 04:07:43 +1000

    Search by worker and service updated on search page (hard coded inputs)

--- FrontEnd/my-app/src/components/SearchAvailability/Searchbar.js
+++ FrontEnd/my-app/src/components/SearchAvailability/Searchbar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import axios from "axios";
 import '../../css/Searchbar.css'
-import Results from './ResultsList';
+import ServiceResultsList from './ServiceSearch/ServiceResultsList'
+import WorkerResultsItem from './WorkerResultsItem';
 
 class Searchbar extends Component {
     
@@ -10,8 +11,13 @@ class Searchbar extends Component {
 
         this.state = {
             searchType: 'service',
-            
 
+            // results listed when searching for a service
+            testServiceRes: ["emp5", "emp6"],
+
+            // results listed when searching for a worker
+            testWorkerRes: ["emp10"],
+            
             custID: 'cus5',
             service: '',
             location: '',
@@ -24,7 +30,7 @@ class Searchbar extends Component {
             value:''
         };
 
-        this.onChangeRadio = this.onChangeRadio.bind(this)
+        this.onChangeSearchType = this.onChangeSearchType.bind(this)
     }
 
 
@@ -36,110 +42,162 @@ class Searchbar extends Component {
 
         e.preventDefault();
         this.state.searched = true;
-
-        const searchQuery = {
-            service: this.state.service,
-            location: this.state.location,
-            worker: this.state.worker,
-            time: this.state.time,
-            date:this.state.date
-        }
         
-        console.log(searchQuery);
+        console.log("searchType: ", this.state.searchType, "\nsearch: ", this.state.search)
 
-        // Validation
+        // service query
+        // should return the list of employees offering the service
+        if(this.state.searchType === 'service') {
+            
+            this.runServiceQuery()
+       }
 
-        if(this.validate()) {
-            // this.props.history.push('/searchResults');
-            const empID = this.state.worker
-            const service = this.state.service
-            const url = 'http://localhost:8080/api/workinghours/list/' + empID + '/' + service
-            this.setState({ loading: true });
-            axios.get(url, {
-            // headers: { 'Authorization': authorization }
-            })
-            .then(res => {
-                console.log(res.data)
-                this.setState({
-                results: res.data,
-                loading: false,
-                searched: true
-                })
-            })
-            .catch((error) => {
-                console.log("error",error)
-            })
+        // worker query
+        // should return the worker's timetable
+        else if(this.state.searchType === 'worker') {
+
+            this.runWorkerQuery()
+
         }
-        else {
-            alert("Invalid search");
-        }
+       
     }
 
-    myChangeHandler = event => {
-        this.setState({[event.target.name]: event.target.value})
+    runWorkerQuery = e => {
+
+      // query the database to check if a worker exists
+
+      let found = false;
+
+      this.state.testWorkerRes.map((emp) => {
+        console.log(emp)
+        if (emp.localeCompare(this.state.search) == 0 && !found) {
+          this.setState({results: emp})
+          found = true;
+          console.log("found")
+        }
+      })
+
+      // test result
+      if(!found)
+        this.setState({results: null})
+
+    }
+
+    runServiceQuery = e => {
+
+      // query the database to retrieve all workers that do the service
+
+      // const searchQuery = {
+      //   service: this.state.service,
+      //   location: this.state.location,
+      //   worker: this.state.worker,
+      //   time: this.state.time,
+      //   date:this.state.date
+      // }
+
+      // if(this.validate()) {
+      //   const empID = this.state.worker
+      //   const service = this.state.service
+      //   const url = 'http://localhost:8080/api/workinghours/list/' + empID + '/' + service
+      //   this.setState({ loading: true });
+      //   axios.get(url, {
+      //   // headers: { 'Authorization': authorization }
+      //   })
+      //   .then(res => {
+      //     console.log(res.data)
+      //     this.setState({
+      //     results: res.data,
+      //     loading: false,
+      //     searched: true
+      //     })
+      //   })
+      //   .catch((error) => {
+      //     console.log("error",error)
+      //   })
+      // }
+      // else {
+      //   alert("Invalid search");
+      // }
+
+      // test result
+      this.setState({results: this.state.testServiceRes});
+    }
+
+    onChangeRadioButtonState = event => {
+
+      this.setState({[event.target.name]: event.target.value})
     };
 
     get results() {
-        let results;
-        if (this.state.searched){
-            results = <h1>There's no available slots</h1>;
-        }
-        
+
+      let results
+      
+      console.log("results: ", this.state.results)
+
+      if(this.state.searchType === "service") {
         if (this.state.results) {
-          results = <Results list={this.state.results} />;
+          results = <ServiceResultsList list={this.state.results}/>
         }
-    
-        return results;
+      }
+
+      else  if(this.state.searchType === "worker") {
+
+        if(this.state.results) {
+          results = <WorkerResultsItem name={this.state.results}/>
+        } else {
+          return (<p>Sorry! We couldn't find a worker by that name</p>)
+        }
+      }
+
+      return results
     }
 
-    onChangeRadio = e => {
+    onChangeSearchType = e => {
 
-        this.setState({searchType: e.target.value}, () => {
-            console.log(this.state.searchType)
-        })
+      this.setState({searchType: e.target.value})
     }
 
     render() {
-        return (
-            <div id='search'>
-                <h1 className="display-4 text-center">Make a Booking</h1>
-                <p>Bookings can be made through a simple search of service, or through the timetable of a preferred, available worker.</p>
-                <form action='' onSubmit={ this.onSubmit }>
-                    <div id='radio-group'>
-                        <input 
-                            id='service' 
-                            type='radio' 
-                            name='search' 
-                            value='service' 
-                            onClick={this.onChangeRadio}
-                        />
-                        <label for='service'>Service</label><br/>
-                        <input 
-                            id='worker' 
-                            type='radio' 
-                            name='search' 
-                            value='worker'
-                            onClick={this.onChangeRadio}
-                        />
-                        <label for='worker'>Worker</label><br/>
-                        <input 
-                            id='search-bar'
-                            name= "search"
-                            value = { this.state.search }
-                            onChange={this.myChangeHandler}
-                            type='text' 
-                            placeholder='Search' 
-                            pattern='[A-Za-z]+' 
-                            required='required'
-                            title='Alphabetical characters only'
-                        />
-                    </div>
-                    <button type='submit'>Search</button>
-                </form>
-                {this.results}
-            </div>
-        )
-    }
+      return (
+      <div id='search'>
+        <h1 className="display-4 text-center">Make a Booking</h1>
+        <p>Bookings can be made through a simple search of service, or through the timetable of a preferred, available worker.</p>
+        <form action='' onSubmit={ this.onSubmit }>
+          <div id='radio-group'>
+            <input 
+              id='service' 
+              type='radio' 
+              name='search' 
+              value='service' 
+              onClick={this.onChangeSearchType}
+              required='required'
+            />
+            <label for='service'>Service</label><br/>
+            <input 
+              id='worker' 
+              type='radio' 
+              name='search' 
+              value='worker'
+              onClick={this.onChangeSearchType}
+            />
+            <label for='worker'>Worker</label><br/>
+            <input 
+              id='search-bar'
+              name= "search"
+              value = { this.state.search }
+              onChange={this.onChangeRadioButtonState}
+              type='text' 
+              placeholder='Search' 
+              required='required'
+              title='Alphabetical characters only'
+            />
+          </div>
+            <button type='submit'>Search</button>
+        </form>
+        {this.results}
+      </div>
+    )
+  }
 }
 
 export default Searchbar;
